828413c789ece193a8b2badaaeceee0a9621808a
Paul Tagliamonte <tag@pault.ag>
2014-05-28 16:01:15 -0400

    Update URLs

--- upload/urls.py
+++ upload/urls.py
@@ -5,11 +5,7 @@ from django.contrib import admin
 urlpatterns = patterns('upload',
     url(r'^$', 'views.home', name='home'),
     url(r'^queue/$', 'views.queue', name='queue'),
-
     url(r'^guide/$', 'views.guide', name='spreadsheet_guide'),
-
-    url(r'^upload/$', 'views.upload', name='upload'),
     url(r'^migrate/$', 'views.migrate', name='migrate'),
-
     url(r'^manage/(?P<transaction>.*)/$', 'views.manage', name='manage'),
 )
