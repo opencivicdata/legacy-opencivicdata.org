8d082ef24a3bdef3a6f816bdb7914d7a4bfaf9f7
Paul Tagliamonte <tag@pault.ag>
2014-06-05 10:21:12 -0400

    Start replacing home with website.

--- data/urls.py
+++ data/urls.py
@@ -2,13 +2,10 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^$', include('home.urls')),
+    url(r'^$', include('website.urls')),
 
     url(r'^upload/', include('upload.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/$', 'django.contrib.auth.views.login', name="login"),
-
-    url(r'^logout/$', 'sunlightauth.views.logout', name='logout'),
 
     url('', include('social.apps.django_app.urls', namespace='social')),
     url(r'', include('sfapp.urls')),
--- home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-
-
-urlpatterns = patterns('home',
-    url(r'^$', 'views.home', name='home'),
-)
