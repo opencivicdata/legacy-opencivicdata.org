a72272e973921157ba79b95b518508451b80b9b6
Paul Tagliamonte <tag@pault.ag>
2014-06-05 10:41:32 -0400

    Pass off root to website

--- data/urls.py
+++ data/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^$', include('website.urls')),
+    url(r'^', include('website.urls')),
 
     url(r'^upload/', include('upload.urls')),
     url(r'^admin/', include(admin.site.urls)),
