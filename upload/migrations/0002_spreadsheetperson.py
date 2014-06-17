559eeb1e4012e1c84396e31af57b1e4fd637ca3c
Paul Tagliamonte <tag@pault.ag>
2014-06-17 13:57:05 -0400

    Add in upload migrations.

---
+++ upload/migrations/0002_spreadsheetperson.py
@@ -0,0 +1,30 @@
+# encoding: utf8
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('upload', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='SpreadsheetPerson',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True, serialize=False)),
+                ('name', models.TextField()),
+                ('image', models.TextField()),
+                ('party', models.TextField(blank=True)),
+                ('position', models.TextField()),
+                ('district', models.TextField()),
+                ('spreadsheet', models.ForeignKey(to_field='id', to='upload.SpreadsheetUpload')),
+                ('code', models.TextField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
