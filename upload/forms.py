80ca86abbfe8c6dc965771f21446b7f8c409081a
Paul Tagliamonte <tag@pault.ag>
2014-07-10 14:02:41 -0400

    Change to progressive enhancement.

--- upload/forms.py
+++ upload/forms.py
@@ -7,16 +7,6 @@ from django.forms import widgets
 from django import forms
 
 
-class Select2Widget(widgets.TextInput):
-    def render(self, name, value, attrs=None):
-        return mark_safe("""
-                         <input type="hidden"
-                                class="bigdrop"
-                                name="{}" id="{}"
-                                style="width:600px" />
-                         """.format(name, name))
-
-
 class JurisdictionModelChoiceField(forms.ModelChoiceField):
     def label_from_instance(self, obj):
         return obj.id
@@ -26,7 +16,6 @@ class SpreadsheetUploadForm(forms.Form):
     jurisdiction = JurisdictionModelChoiceField(
         empty_label=None,
         queryset=Jurisdiction.objects.all(),
-        widget=Select2Widget,
     )
     source = forms.CharField()
     sheet = forms.FileField()
