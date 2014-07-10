279199c632a62e1cf62e44de161361e71c697745
Paul Tagliamonte <tag@pault.ag>
2014-07-10 11:52:53 -0400

    Add in select2, jquery.

--- upload/forms.py
+++ upload/forms.py
@@ -1,6 +1,20 @@
 from opencivicdata.models import Jurisdiction
 
+from django.template import Context
+from django.template.loader import get_template
+from django.utils.safestring import mark_safe
+from django.forms import widgets
 from django import forms
+
+
+class Select2Widget(widgets.TextInput):
+    def render(self, name, value, attrs=None):
+        return mark_safe(get_template(
+            'data/upload/forms/select2.html'
+        ).render(Context({
+            "name": name,
+            "value": value,
+        })))
 
 
 class JurisdictionModelChoiceField(forms.ModelChoiceField):
@@ -12,6 +26,7 @@ class SpreadsheetUploadForm(forms.Form):
     jurisdiction = JurisdictionModelChoiceField(
         empty_label=None,
         queryset=Jurisdiction.objects.all(),
+        widget=Select2Widget,
     )
     source = forms.CharField()
     sheet = forms.FileField()
