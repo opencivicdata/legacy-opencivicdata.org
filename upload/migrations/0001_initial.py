de3f80e86811fbc11f6fa8db194200b2c49f5c99
Paul Tagliamonte <tag@pault.ag>
2014-09-05 12:15:02 -0400

    Force override Django's insitance on a new migration.

--- upload/migrations/0001_initial.py
+++ upload/migrations/0001_initial.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('opencivicdata', '0002_auto_20140905_1338'),
+        ('opencivicdata', '0001_initial'),
     ]
 
     operations = [
