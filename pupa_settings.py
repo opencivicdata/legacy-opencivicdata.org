b98087ff3475c9abd70399677e9f2ba1c7772a7f
Paul Tagliamonte <tag@pault.ag>
2014-09-05 13:51:29 -0400

    Use test API instance.

--- pupa_settings.py
+++ pupa_settings.py
@@ -1,4 +1,4 @@
 import os
 DATABASE_URL = os.environ.get('DATABASE_URL', 'postgis://opencivicdata:test@10.42.2.101/opencivicdata')
 
-OCD_API_HOST = "http://localhost:8080"
+OCD_API_HOST = "http://10.42.2.102"
