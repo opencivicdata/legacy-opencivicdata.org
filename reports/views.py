5a77e17f1c98ebf8b184f28052787b6902d4993a
Miles Watkins <miles.w.watkins@gmail.com>
2015-05-20 10:50:29 -0400

    Change nomenclature from 'measures' to 'checks'

--- reports/views.py
+++ reports/views.py
@@ -12,36 +12,36 @@ def report(request):
     # Identify the most recent plans for each jurisdiction
     newest_plans_by_jurisdiction = RunPlan.objects.values('jurisdiction').annotate(newest_plan=Max('id')).values_list('newest_plan', flat=True)
 
-    # Identify all measures that have been run in any of these reports
-    newest_measures_by_jurisdiction = DataQualityChecks.objects.filter(plan__id__in=newest_plans_by_jurisdiction)
-    all_measures = set([(x.type.object_type, x.type.name) for x in newest_measures_by_jurisdiction])
-    all_measures = sorted(sorted(all_measures, key=lambda x: x[1]), key=lambda x: x[0])
+    # Identify all checks that have been run in any of these reports
+    newest_checks_by_jurisdiction = DataQualityChecks.objects.filter(plan__id__in=newest_plans_by_jurisdiction)
+    all_checks = set([(x.type.object_type, x.type.name) for x in newest_checks_by_jurisdiction])
+    all_checks = sorted(sorted(all_checks, key=lambda x: x[1]), key=lambda x: x[0])
 
-    # Split the measures into categories by jurisdiction and object type
-    measures_by_jurisdiction = {}
+    # Split the checks into categories by jurisdiction and object type
+    checks_by_jurisdiction = {}
 
     for jurisdiction in newest_plans_by_jurisdiction:
-        measures = DataQualityChecks.objects.filter(plan__id=jurisdiction)
+        checks = DataQualityChecks.objects.filter(plan__id=jurisdiction)
 
-        # Initialize all measures, in case divisions differ in which they use
-        measures_by_type = OrderedDict()
-        for measure in all_measures:
-            if measure[0] not in measures_by_type:
-                measures_by_type[measure[0]] = OrderedDict()
-            measures_by_type[measure[0]][measure[1]] = None
+        # Initialize all checks, in case divisions differ in which they use
+        checks_by_type = OrderedDict()
+        for check in all_checks:
+            if check[0] not in checks_by_type:
+                checks_by_type[check[0]] = OrderedDict()
+            checks_by_type[check[0]][check[1]] = None
 
-        for measure in measures:
-            measures_by_type[measure.type.object_type][measure.type.name] = measure.value
+        for check in checks:
+            checks_by_type[check.type.object_type][check.type.name] = check.value
 
         jurisdiction_name = RunPlan.objects.get(id=jurisdiction).jurisdiction
-        measures_by_jurisdiction[jurisdiction_name] = measures_by_type
+        checks_by_jurisdiction[jurisdiction_name] = checks_by_type
 
     object_types = OrderedDict()
-    for measure in all_measures:
-        object_types[measure[0]] = object_types.get(measure[0], 0) + 1
+    for check in all_checks:
+        object_types[check[0]] = object_types.get(check[0], 0) + 1
 
     return render(request, "data/reports/reports.html", {
         "object_types": object_types,
-        "measures": [x[1] for x in all_measures],
-        "measures_by_jurisdiction": measures_by_jurisdiction,
+        "checks": [x[1] for x in all_checks],
+        "checks_by_jurisdiction": checks_by_jurisdiction,
     })
